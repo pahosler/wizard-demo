8dc325b420a9aaa43d84d64f889bcd301e4acfb2
pahosler <pahosler@gmail.com>
2018-10-04 21:49:26 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Wizard from '@rahsheen/react-wizard';
+import MakeTimer from 'maketimer';
 import './App.css';
+import Header from './Header.js'
 
+const timer = new MakeTimer();
+timer.start(1000, () => {
+  console.log('HI');
+});
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Wizard>
+          <Wizard.Step>
+            {({ nextStep, prevStep }) => {
+              return (
+                <div>
+                <Header />
+                  <div>
+                    <h2>This is Step 1</h2>
+                    <button onClick={nextStep}>Next</button>
+                  </div>
+                </div>
+              );
+            }}
+          </Wizard.Step>
+          <Wizard.Step>
+            {({ submit, prevStep }) => (
+              <div>
+              <Header />
+                <h2>This is Step 2</h2>
+                <button onClick={prevStep}>Back</button>
+                <button onClick={onsubmit}>Finish</button>
+              </div>
+            )}
+          </Wizard.Step>
+        </Wizard>
       </div>
     );
   }
